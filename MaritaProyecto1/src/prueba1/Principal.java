121b276b7bf2db70d22b0596ac27b1abd6678306
MaritaSicilia <90844940+MaritaSicilia@users.noreply.github.com>
2022-03-30 09:22:41 +0200

    Paso 2. Modifico el syso añadiendo mayúsculas

--- MaritaProyecto1/src/prueba1/Principal.java
+++ MaritaProyecto1/src/prueba1/Principal.java
@@ -3,7 +3,7 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo Java-Git");
 	}
 
 }
